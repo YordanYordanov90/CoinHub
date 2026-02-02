6f196eab6c8b330c6b5566f6d5656b11d2b64dc2
YordanYordanov90 <157980191+YordanYordanov90@users.noreply.github.com>
2026-02-02 14:08:43 +0200

    homepage initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import CoinOverview from '@/components/home/coin-overview'
+import {
+  CoinOverviewFallback,
+  TrendingCoinsFallback,
+} from '@/components/home/fallback'
+import TrendingCoins from '@/components/home/trending-coins'
+import { Suspense } from 'react'
 
-const HomePage = () => {
+const HomePage = async () => {
   return (
-    <div>HomePage</div>
+    <main className="min-h-screen bg-linear-to-r from-zinc-900 to-zinc-800 text-white">
+      <div className="container mx-auto px-4 py-8 gap-8 grid grid-cols-1 md:grid-cols-2">
+        <section className="mb-12">
+          <Suspense fallback={<CoinOverviewFallback />}>
+            <CoinOverview />
+          </Suspense>
+        </section>
+        <section className="mb-12">
+          <Suspense fallback={<TrendingCoinsFallback />}>
+            <TrendingCoins />
+          </Suspense>
+        </section>
+      </div>
+    </main>
   )
 }
 
